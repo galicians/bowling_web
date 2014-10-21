4bb271068650bb0d23d86c236baf117060e5de71
galicians <galicians@gmail.com>
2014-10-21 16:22:41 +0100

    background added, style improved, logic moved to js file

--- js/frontEndLogic.js
+++ js/frontEndLogic.js
@@ -2,6 +2,46 @@
 
 // game = new Game
 
+			game = new Game
+	
+	iteratePins()
+	// game.rollTheBall(5)
+	// iteratePins()
+
+	function iteratePins() {
+		var pins = game.currentFrame.pins
+		for(var p = 0; p < pins.length; p++) {
+
+			displayPins(pins[p])
+		}
+	}
+	
+	function randThrow() {
+		var remainingPins = game.currentFrame.numberRemainingPins()
+		return Math.floor(Math.random()*remainingPins)
+	}
+	
+	$('img.category').on('click', function() { $(this).animate({ opacity: 0.0, paddingLeft: '+=80'}, 500, function() { $(this).remove()})})
+
+
+
+	function displayPins(p) {
+		var pin = document.createElement("img")
+			p.isKnocked ? pin.setAttribute("src", "images/pinDown.jpeg") : pin.setAttribute("src", "images/bowlingpin.png")
+			pin.align="center"
+			pin.class="category"
+			pin.id="bowlingpin"
+
+		// pin.setAttribute("width", "50")
+		// pin.setAttribute("height", "50")
+		document.body.appendChild(pin)
+	// }
+
+
+
+
+
+
 $('#bowlingball').on('click', function() { $(this).animate({ opacity: 0.0, paddingLeft: '+=80'}, 500, function() { 
 	$(this).remove()
 	randNumber = randThrow()
